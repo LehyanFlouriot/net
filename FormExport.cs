c29582d5f9297b9989fbcec13c1dc379d1db6acd
LehyanFlouriot <lehyan.flouriot06@gmail.com>
2022-04-07 14:03:04 +0200

    exportmarche

--- FormExport.cs
+++ FormExport.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SQLite;
+using System.IO;
 
 namespace Hector
 {
@@ -58,7 +59,7 @@ namespace Hector
 
         private void ButtonExport_Click(object sender, EventArgs e)
         {
-            
+            /*
             List<Marque> Marques = DataBase.GetMarques("Hector.SQLite");
             Console.WriteLine(Marques.Count);
             List<Famille> Familles = DataBase.GetFamilles("Hector.SQLite");
@@ -85,15 +86,37 @@ namespace Hector
                 Console.WriteLine("Article Desc : "+Article.Description+" Ref : " + Article.RefArticle + " Marque : " + NomMarque + " Famille : " + NomFamille + " Sous famille : " + NomSousFamille+ " Prix : " + Article.PrixHT);
 
             }
-            /*
-            SQLiteCommand Command = new SQLiteCommand("Select Description, RefArticle, Marques.Nom, Familles.Nom, SousFamilles.Nom, PrixHT from Articles inner join Marques on Articles.RefMarque = Marques.RefMarque inner join SousFamilles on Articles.RefSousFamille = SousFamilles.RefSousFamille inner join Familles on SousFamilles.RefFamille = Familles.RefFamille", DataBase.Conn);
-            
-            SQLiteDataReader Reader = Command.ExecuteReader();
-            while (Reader.Read())
-            {
-                Console.WriteLine(Reader.GetString(0));
-            }
             */
+            SQLiteCommand Command = new SQLiteCommand("Select Description, RefArticle, Marques.Nom, Familles.Nom, SousFamilles.Nom, PrixHT from Articles inner join Marques on Articles.RefMarque = Marques.RefMarque inner join SousFamilles on Articles.RefSousFamille = SousFamilles.RefSousFamille inner join Familles on SousFamilles.RefFamille = Familles.RefFamille", DataBase.Conn);
+            SQLiteDataReader Reader = Command.ExecuteReader();
+
+            
+
+            using (StreamWriter sw = File.CreateText(ExportFolderPath + "\\export.csv"))
+            {
+                try
+                {
+                    sw.WriteLine("Description; Ref; Marque; Famille; Sous - Famille; Prix H.T.");
+
+                    while (Reader.Read())
+                    {
+                        string Description = Reader.GetString(0);
+                        string Ref = Reader.GetString(1);
+                        string Marque = Reader.GetString(2);
+                        string Famille = Reader.GetString(3);
+                        string SousFamille = Reader.GetString(4);
+                        string PrixHT = Reader.GetString(5);
+
+                        sw.WriteLine(Description + ";" + Ref + ";" + Marque + ";" + Famille + ";" + SousFamille + ";" + PrixHT);
+                        Console.WriteLine(Description + ";" + Ref + ";" + Marque + ";" + Famille + ";" + SousFamille + ";" + PrixHT);
+                    }
+                    sw.Close();
+                }
+                catch (Exception er)
+                {
+                    Console.WriteLine("Exception: " + er.Message);
+                }
+            }
         }
     }
 }
